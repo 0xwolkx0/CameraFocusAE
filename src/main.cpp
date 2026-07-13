5ca1e626fad0a665bd91456ea6f01c7e8da8b37a
0xwolkx0 <s.chuprun@yandex.ru>
2026-07-13 17:05:11 +0200

    Kinda works now

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <keyhandler/keyhandler.h>
 #include "CameraManager.h"
+#include "RE/N/NiMatrix3.h"
+#include "RE/N/NiPoint3.h"
 
 using UpdateFunc = void(RE::ThirdPersonState*, RE::BSTSmartPointer<RE::TESCameraState>&);
 std::uintptr_t _OriginalUpdate = 0;
 CameraFocusState currentFocus = CameraFocusState::Default;
 RE::NiPoint3 defaultOffset;
+RE::NiPoint3 neckPos;
+RE::NiPoint3 rootPos;
+
 void HookedUpdate(RE::ThirdPersonState* a_this, RE::BSTSmartPointer<RE::TESCameraState>& a_nextState);
 
 static void SKSEMessageHandler(SKSE::MessagingInterface::Message* message)
@@ -36,6 +41,72 @@ RE::NiPointer<RE::NiCamera> GetNiCamera(RE::PlayerCamera* camera)
     }
     return nullptr;
 }
+void LogPlayerBonesAndFindNearNeck()
+{
+    auto player = RE::PlayerCharacter::GetSingleton();
+    if (!player || !player->Get3D()) {
+        logger::info("Player or 3D not available");
+        return;
+    }
+
+    auto root = player->Get3D();
+
+    // First, find the neck bone to get reference Z coordinate
+    auto neckNode = root->GetObjectByName("NPC Neck [Neck]");
+    if (!neckNode) {
+        logger::info("Neck bone not found");
+        return;
+    }
+
+    float neckZ = neckNode->world.translate.z;
+    logger::info("=== Neck bone Z coordinate: {:.3f} ===", neckZ);
+    logger::info("");
+
+    // Collect all bones
+    std::vector<RE::NiAVObject*> allBones;
+    std::function<void(RE::NiAVObject*)> collectBones = [&](RE::NiAVObject* node) {
+        if (!node) return;
+
+        allBones.push_back(node);
+
+        auto asNode = node->AsNode();
+        if (asNode) {
+            for (auto& child : asNode->children) {
+                if (child) {
+                    collectBones(child.get());
+                }
+            }
+        }
+    };
+
+    collectBones(root);
+
+    // Log all bones
+    logger::info("=== All Player Bones ({} total) ===", allBones.size());
+    for (auto* bone : allBones) {
+        auto pos = bone->world.translate;
+        logger::info("{}: ({:.3f}, {:.3f}, {:.3f})",
+            bone->name.c_str(),
+            pos.x, pos.y, pos.z);
+    }
+
+    logger::info("");
+
+    // Find bones with similar Z to neck (within ±3)
+    logger::info("=== Bones near neck Z coordinate ({:.3f} ± 3) ===", neckZ);
+    for (auto* bone : allBones) {
+        float boneZ = bone->world.translate.z;
+        float diff = std::abs(boneZ - neckZ);
+
+        if (diff <= 3.0f) {
+            auto pos = bone->world.translate;
+            logger::info("{}: ({:.3f}, {:.3f}, {:.3f}) [diff: {:.3f}]",
+                bone->name.c_str(),
+                pos.x, pos.y, pos.z,
+                diff);
+        }
+    }
+}
 
 void CycleState() {
     auto playerCamera = RE::PlayerCamera::GetSingleton();
@@ -49,40 +120,36 @@ void CycleState() {
         logger::info("Not in third person view, camera cycling disabled");
         return;
     }
-    auto tps = static_cast<RE::ThirdPersonState*>(playerCamera->currentState.get());
+    //auto tps = static_cast<RE::ThirdPersonState*>(playerCamera->currentState.get());
     auto player = RE::PlayerCharacter::GetSingleton();
-    RE::NiPoint3 headPos = player->Get3D()->AsNode()->GetObjectByName("NPC Head [Head]")->world.translate;
-    RE::NiPoint3 headLocalPos = player->Get3D()->AsNode()->GetObjectByName("NPC Head [Head]")->local.translate;
-    RE::NiPoint3 pelvisPos = player->Get3D()->AsNode()->GetObjectByName("NPC Pelvis [Pelv]")->world.translate;
-    RE::NiPoint3 pelvisLocalPos = player->Get3D()->AsNode()->GetObjectByName("NPC Pelvis [Pelv]")->local.translate;
+    neckPos = player->Get3D()->AsNode()->GetObjectByName("NPC Neck [Neck]")->world.translate;
+    neckPos = {neckPos.x,neckPos.y,neckPos.z};
+    rootPos = player->Get3D()->AsNode()->GetObjectByName("NPC Root [Root]")->world.translate;
+    auto niCamera = GetNiCamera(playerCamera);
+    auto tps = skyrim_cast<RE::ThirdPersonState*>(playerCamera->currentState.get());
+    //tps->posOffsetActual = tps-> posOffsetExpected = {0,0,0};
     // Cycle to next state
     switch (currentFocus) {
         case CameraFocusState::Default:
             defaultOffset = tps->posOffsetActual;
-            logger::info("Camera default position: World ({}, {}, {}) Local ({}, {}, {})", GetNiCamera(playerCamera)->world.translate.x, GetNiCamera(playerCamera)->world.translate.y, GetNiCamera(playerCamera)->world.translate.z, GetNiCamera(playerCamera)->local.translate.x, GetNiCamera(playerCamera)->local.translate.y, GetNiCamera(playerCamera)->local.translate.z);
-
             currentFocus = CameraFocusState::Head;
             tps->posOffsetExpected = {0,0,0};
             logger::info("Camera state: Head");
             break;
         case CameraFocusState::Head:
-            logger::info("Camera Head coordinates: World ({}, {}, {}) Local ({}, {}, {})", GetNiCamera(playerCamera)->world.translate.x, GetNiCamera(playerCamera)->world.translate.y, GetNiCamera(playerCamera)->world.translate.z, GetNiCamera(playerCamera)->local.translate.x, GetNiCamera(playerCamera)->local.translate.y, GetNiCamera(playerCamera)->local.translate.z);
-            logger::info("Head position: ({}, {}, {})", headPos.x, headPos.y, headPos.z);
-            logger::info("Head local position: ({}, {}, {})", headLocalPos.x, headLocalPos.y, headLocalPos.z);
             currentFocus = CameraFocusState::Pelvis;
             tps->posOffsetExpected = {0,0,0};
             logger::info("Camera state: Pelvis");
             break;
         case CameraFocusState::Pelvis:
-            logger::info("Camera Pelvis coordinates: World ({}, {}, {}) Local ({}, {}, {})", GetNiCamera(playerCamera)->world.translate.x, GetNiCamera(playerCamera)->world.translate.y, GetNiCamera(playerCamera)->world.translate.z, GetNiCamera(playerCamera)->local.translate.x, GetNiCamera(playerCamera)->local.translate.y, GetNiCamera(playerCamera)->local.translate.z);
-            logger::info("Pelvis position: ({}, {}, {})", pelvisPos.x, pelvisPos.y, pelvisPos.z);
-            logger::info("Pelvis local position: ({}, {}, {})", pelvisLocalPos.x, pelvisLocalPos.y, pelvisLocalPos.z);
             currentFocus = CameraFocusState::Default;
             tps->posOffsetExpected = defaultOffset;
             logger::info("Camera state: Default");
             break;
     }
 }
+
+
 
 void InstallThirdPersonUpdateHook()
 {
@@ -101,10 +168,10 @@ void HookedUpdate(RE::ThirdPersonState* a_this, RE::BSTSmartPointer<RE::TESCamer
     originalFunc(a_this, a_nextState);
 
     auto playerCamera = RE::PlayerCamera::GetSingleton();
-    auto& cameraNode = playerCamera->cameraRoot;
 
     if(currentFocus != CameraFocusState::Default){
         // Now modify the camera transform to override what the game just did
+        RE::NiMatrix3 rotation = GetNiCamera(playerCamera)->world.rotate;
         auto focusBoneName = "";
         switch(currentFocus){
             case CameraFocusState::Head:
@@ -116,8 +183,12 @@ void HookedUpdate(RE::ThirdPersonState* a_this, RE::BSTSmartPointer<RE::TESCamer
         }
         auto player = RE::PlayerCharacter::GetSingleton();
         RE::NiPoint3 bonePos = player->Get3D()->AsNode()->GetObjectByName(focusBoneName)->world.translate;
-        auto currentPos = GetNiCamera(playerCamera)->world.translate;
-        GetNiCamera(playerCamera)->world.translate = {(currentPos.x*2) - bonePos.x, (currentPos.y*2) - bonePos.y, (currentPos.z*2) - bonePos.z};
+        //RE::NiPoint3 currentPos = GetNiCamera(playerCamera)->world.translate;
+
+        RE::NiPoint3 rootPosOffset = {rootPos.x, rootPos.y, neckPos.z}; 
+        
+        //GetNiCamera(playerCamera)->world.rotate = rotation;
+        GetNiCamera(playerCamera)->world.translate -= rootPosOffset - bonePos;
         //cameraNode->local.translate = cameraNode->world.translate = GetNiCamera(playerCamera)->world.translate = bonePos;
 /*         if (playerCamera->currentState->id == RE::CameraState::kThirdPerson)
             skyrim_cast<RE::ThirdPersonState*>(playerCamera->currentState.get())->translation = cameraNode->local.translate; */
